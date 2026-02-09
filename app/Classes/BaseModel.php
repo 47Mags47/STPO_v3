f0281b9a2604be9b2c49f9097f1fa69d09817e78
47Mags47 <gord_ka47@mail.ru>
2026-02-09 15:09:38 +0700

    load

---
+++ app/Classes/BaseModel.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Classes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Str;
+
+abstract class BaseModel extends Model
+{
+    ### Методы
+    ##################################################
+    public static function getTableName()
+    {
+        return with(new static)->getTable();
+    }
+
+    protected static function getGuessNames(?string $name = null): array| string| bool
+    {
+        $modelClass = static::class;
+
+        if (! Str::contains($modelClass, '\\Models\\')) {
+            return [];
+        }
+
+        $relativeNamespace = Str::after($modelClass, '\\Models\\');
+        $path = Str::before($modelClass, 'App\\Models');
+
+        $names = [
+            'class'             => $modelClass,
+            'className'         => $relativeNamespace,
+            'path'              => $path,
+            'factory'           => self::getClassKnowingFolderAndEnding('Database\\Factories', $relativeNamespace, 'Factory'),
+            'localseeder'       => self::getClassKnowingFolderAndEnding('Database\\Seeders\\Local', $relativeNamespace, 'Seeder'),
+            'prodseeder'        => self::getClassKnowingFolderAndEnding('Database\\Seeders\\Prod', $relativeNamespace, 'Seeder'),
+            'filter'            => self::getClassKnowingFolderAndEnding('Database\\Filters', $relativeNamespace, 'Filter'),
+            'controller'        => self::getClassKnowingFolderAndEnding('App\\Http\\Controllers', $relativeNamespace, 'Controller'),
+            'policy'            => self::getClassKnowingFolderAndEnding('App\\Policies', $relativeNamespace, 'Policy'),
+            'storerequest'      => self::getClassKnowingFolderAndEnding('App\\Http\\Requests', 'Store' . $relativeNamespace, 'Request'),
+            'updaterequest'     => self::getClassKnowingFolderAndEnding('App\\Http\\Requests', 'Update' . $relativeNamespace, 'Request'),
+            'resource'          => self::getClassKnowingFolderAndEnding('App\\Http\\Resources', $relativeNamespace, 'Resource'),
+        ];
+
+        return $name !== null
+            ? $names[$name]
+            : $names;
+    }
+
+    public function scopeFilter(Builder $builder): Builder
+    {
+        return self::getGuessNames('filter')
+            ? new (self::getGuessNames('filter'))($builder)->apply()
+            : new \App\Classes\Filter($builder)->apply();
+    }
+
+    public static function getResource()
+    {
+        $query = self::Filter();
+
+        $data = (request()->has('paginate') and (int) request()->input('paginate') > 0)
+            ? $query->paginate(request()->input('paginate'))
+            : $query->get();
+
+        return self::getGuessNames('resource')
+            ? self::getGuessNames('resource')::collection($data)
+            : $data->toResourceCollection();
+    }
+
+    public static function findFromArray(array $array) // DELETE
+    {
+        return self::where(function ($query) use ($array) {
+            foreach ($array as $column => $value) {
+                if (!in_array($column, new static()->getHidden()) and in_array($column, new static()->getFillable()))
+                    $query->where($column, $value);
+            }
+        })->get();
+    }
+
+    public static function randomOrCreate(array $attributes = [])
+    {
+        return self::count() > 0
+            ? self::all()->random()
+            : self::factory()->create($attributes);
+    }
+
+    private static function getClassKnowingFolderAndEnding(string $namespace, string $class, string $ending = ''): string|bool
+    {
+        if (class_exists($namespace . '\\' . $class . $ending))
+            return $namespace . '\\' . $class . $ending;
+
+        if (class_exists($namespace . '\\' . $class))
+            return $namespace . '\\' . $class;
+
+        return false;
+    }
+}
