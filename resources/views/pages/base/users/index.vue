b8afd910d28b49d5ece58357cf81117697dbf0e3
DavidKaigorodov <bossdilan2003@gmail.com>
2026-02-27 16:41:06 +0700

    Merge commit 'f6c570a37f8de6f70d97b6c7922c1488e5ce02d7'

--- resources/views/pages/base/users/index.vue
+++ resources/views/pages/base/users/index.vue
@@ -1,9 +1,56 @@
 <script>
-export default{
+import { usePage } from "@inertiajs/vue3";
+import ResourceTable from "../../../components/tables/ResourceTable.vue";
 
-}
+export default {
+    components: {
+        ResourceTable,
+    },
+    computed: {
+        users: () => usePage().props.users,
+    },
+    setup() {
+        const columns = [
+            {
+                title: "Имя",
+                dataIndex: "first_name",
+            },
+            {
+                title: "Фамилия",
+                dataIndex: "last_name",
+            },
+            {
+                title: "Фамилия",
+                dataIndex: "middle_name",
+            },
+            {
+                title: "Телефон",
+                dataIndex: "phone",
+            },
+            {
+                title: "Добавочный",
+                dataIndex: "phone_dob",
+            },
+            {
+                title: "Логин",
+                dataIndex: "login",
+            },
+            {
+                title: "Email",
+                dataIndex: "email",
+            },
+        ];
+        return { columns };
+    },
+};
 </script>
 
 <template>
-
+    <ResourceTable
+        header="Пользователи"
+        :columns
+        :data="users"
+        :hasDelete="true"
+        :hasPaginate="true"
+    />
 </template>
