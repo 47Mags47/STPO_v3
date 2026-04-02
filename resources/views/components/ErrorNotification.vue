b4b7301f45f55d67ef083bfdba432ed5a29a66d7
mubait <kuz2001min@mail.ru>
2026-04-02 17:18:45 +0700

    add: * Заглушка вывода уведомления ошибки

---
+++ resources/views/components/ErrorNotification.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+
+}
+</script>
+
+<template>
+
+    <button class="absolute left-[50%] top-[50%] w-32 h-16 bg-cyan-400 rounded cursor-pointer" > показать ошибку </button>
+
+
+
+    <div class="absolute flex justify-center items-center w-[15%] h-[8%] bg-black/80 rounded-md bottom-3 right-3">
+        <p class="flex !text-2xl text-red-600"> какая-то ошибка </p>
+    </div>
+
+</template>
