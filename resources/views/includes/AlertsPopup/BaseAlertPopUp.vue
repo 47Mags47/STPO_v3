7bef1d43a0d0233fb4a75e53fe5ac3f8458e2b80
mubait <kuz2001min@mail.ru>
2026-06-01 11:16:19 +0700

    feat: * алерты попапы готовы

---
+++ resources/views/includes/AlertsPopup/BaseAlertPopUp.vue
@@ -0,0 +1,40 @@
+<script>
+
+</script>
+
+<template>
+    <div class="rounded-lg w-fit max-w-[800px] min-w-[200px] h-fit overflow-hidden drop-shadow-lg">
+        <div class="relative w-full h-[28px] z-10">
+            <slot name="header" />
+        </div>
+
+        <div class="relative custom-scrollbar w-full h-fit">
+            <slot name="content" />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.custom-scrollbar {
+    overflow-y: auto;
+    &::-webkit-scrollbar {
+        width: 4px;
+    }
+
+    &::-webkit-scrollbar-track {
+        background: #3d9bd16c;
+        border-radius: 10px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background: #3d9ad1;
+        border-radius: 10px;
+
+        &:hover {
+            background: #6cbbe9;
+        }
+    }
+}
+
+
+</style>
