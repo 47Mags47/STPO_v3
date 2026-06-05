f43d3cca357a2679c6bcfb2acf11ce9c51084598
47Mags47 <gord_ka47@mail.ru>
2026-06-05 17:26:36 +0700

    Fixs

--- app/Http/Requests/Payment/PaymentUpdateRequest.php
+++ app/Http/Requests/Payment/PaymentUpdateRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Payment;
 
+use App\Models\FSD\Payment;
 use Illuminate\Foundation\Http\FormRequest;
 
 class PaymentUpdateRequest extends FormRequest
@@ -9,8 +10,8 @@ class PaymentUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'code'  => ['required', 'string', 'max:255'],
-            'name'  => ['required', 'string', 'max:255'],
+            'code'  => ['required', 'string', 'max:255', 'unique:'. Payment::class .',code,' . $this->route('payment')->id],
+            'name'  => ['required', 'string', 'max:255', 'unique:'. Payment::class .',name,' . $this->route('payment')->id],
             'kbk'   => ['required', 'string', 'regex:/[0-9]{20}/'],
         ];
     }
