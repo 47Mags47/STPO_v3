b70c0f5a7955dd49caad208aa9614b32882a3bd6
47Mags47 <gord_ka47@mail.ru>
2026-06-23 14:57:23 +0700

    fixs: * Переделал компонент иконок * поправил app.sass

--- resources/views/components/Ico.vue
+++ resources/views/components/Ico.vue
@@ -1,30 +1,35 @@
 <script>
-import * as icons from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const icons = import.meta.glob('/node_modules/@fortawesome/free-solid-svg-icons/fa*.js', { eager: false })
 
 export default {
     components: {
         FontAwesomeIcon,
     },
+
     props: {
         type: {
             type: String,
-            validator(value){
-                return value in icons
-            }
-        },
-    },
-    data() {
-        return {
-            icons
         }
     },
+
+    data(){
+        return {
+            icon: {}
+        }
+    },
+
+    async mounted(){
+        this.icon = await icons[`/node_modules/@fortawesome/free-solid-svg-icons/${this.type}.js`]()
+    }
 };
 </script>
 
 <template>
     <div class="ico-container">
-        <FontAwesomeIcon :icon="icons[type]" />
+        <FontAwesomeIcon :icon="icon.definition"/>
     </div>
 </template>
 
