242033ba82585d0dd89e4239a70ec5d66c3d6384
mubait <kuz2001min@mail.ru>
2026-07-22 11:22:16 +0700

    upd: фильтры работают

--- app/Filters/Appeal/AppealFilter.php
+++ app/Filters/Appeal/AppealFilter.php
@@ -9,4 +9,32 @@ class AppealFilter extends Filter
     public function statusIds(array $ids){
         return $this->builder->whereIn('status_id', $ids);
     }
+
+    public function themIds(array $ids){
+        return $this->builder->whereIn('them_id', $ids);
+    }
+
+    public function senderIds(array $ids){
+        return $this->builder->whereIn('sender_id', $ids);
+    }
+
+    public function createdAt(array $date)
+    {
+        $from = $date['_from'] ?? null;
+        $to   = $date['_to'] ?? null;
+
+        if ($from && $to) {
+            return $this->builder->whereBetween('created_at', [$from, $to]);
+        }
+
+        if ($from) {
+            return $this->builder->whereDate('created_at', '>=', $from);
+        }
+
+        if ($to) {
+            return $this->builder->whereDate('created_at', '<=', $to);
+        }
+
+        return $this->builder;
+}
 }
