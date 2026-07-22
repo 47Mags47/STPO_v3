37c44b7117f2f5770c8333c81f59a3266759f158
mubait <kuz2001min@mail.ru>
2026-07-22 11:56:32 +0700

    upd: сгруппировал тему "темы нет в списке" в одну группу "прочее", из остальных групп эту тему убрал

--- database/seeders/Prod/Appeal/ThemSeeder.php
+++ database/seeders/Prod/Appeal/ThemSeeder.php
@@ -25,22 +25,18 @@ class ThemSeeder extends Seeder
         Them::firstOrCreate(['group_id' => ThemGroup::byCode('technic')->id,   'name' => 'Неполадки с компьютером']);
         Them::firstOrCreate(['group_id' => ThemGroup::byCode('technic')->id,   'name' => 'Неполадки с МФУ | принтером']);
         Them::firstOrCreate(['group_id' => ThemGroup::byCode('technic')->id,   'name' => 'Неполадки с IP-телефоном']);
-        Them::firstOrCreate(['group_id' => ThemGroup::byCode('technic')->id,   'name' => 'Моей темы нет в списке']);
 
         Them::firstOrCreate(['group_id' => ThemGroup::byCode('asp')->id,       'name' => 'Объединение ПКУ']);
         Them::firstOrCreate(['group_id' => ThemGroup::byCode('asp')->id,       'name' => 'Адреса']);
         Them::firstOrCreate(['group_id' => ThemGroup::byCode('asp')->id,       'name' => 'Тарифы ЖКУ']);
-        Them::firstOrCreate(['group_id' => ThemGroup::byCode('asp')->id,       'name' => 'Моей темы нет в списке']);
 
         Them::firstOrCreate(['group_id' => ThemGroup::byCode('security')->id,  'name' => 'ЭЦП']);
         Them::firstOrCreate(['group_id' => ThemGroup::byCode('security')->id,  'name' => 'Антивирус']);
         Them::firstOrCreate(['group_id' => ThemGroup::byCode('security')->id,  'name' => 'Континент']);
         Them::firstOrCreate(['group_id' => ThemGroup::byCode('security')->id,  'name' => 'VIPNET']);
-        Them::firstOrCreate(['group_id' => ThemGroup::byCode('security')->id,  'name' => 'Моей темы нет в списке']);
 
         Them::firstOrCreate(['group_id' => ThemGroup::byCode('transport')->id, 'name' => 'WP']);
         Them::firstOrCreate(['group_id' => ThemGroup::byCode('transport')->id, 'name' => 'Карта жителя']);
-        Them::firstOrCreate(['group_id' => ThemGroup::byCode('transport')->id, 'name' => 'Моей темы нет в списке']);
 
         Them::firstOrCreate(['group_id' => ThemGroup::byCode('other')->id,     'name' => 'Моей темы нет в списке']);
     }
