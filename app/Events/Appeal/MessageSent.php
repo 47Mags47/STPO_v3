e2fc157bd296c4e857115b906ea407e59e5cc134
mubait <kuz2001min@mail.ru>
2026-07-28 16:50:54 +0700

    fix: имя файла с бродкаста опсылалось хэшированное

--- app/Events/Appeal/MessageSent.php
+++ app/Events/Appeal/MessageSent.php
@@ -31,7 +31,12 @@ class MessageSent implements ShouldBroadcastNow
             'message'    => $this->message->message,
             'sender_id'  => $this->message->sender_id,
             'created_at' => $this->message->created_at,
-            'file'       => $this->message->file,
+            'file' => $this->message->file !== null
+                ? [
+                    'id' => $this->message->file->id,
+                    'name' => $this->message->file->origin_name,
+                ]
+                : null,
             'file_url'   => $this->message->file !== null
                 ? route('files.show', ['file' => $this->message->file->id])
                 : null,
