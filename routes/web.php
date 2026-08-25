3681550bd5ed03ee7c2f42913cd0571e24b90490
47Mags47 <gord_ka47@mail.ru>
2026-08-25 13:45:00 +0700

    dev

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,11 @@
 <?php
 
-use App\Models\Administrate\Bank;
-use App\Models\Administrate\Template;
-use App\Models\Payment\BankRaport;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 // DEV
 if (config('app.env') === 'local') {
-    Route::get('/test', function(){
-        $template = Template::where('writer', 'App\Writers\Payment\VTBWriter')->get()->first();
-        $bank = Bank::where('code', 'test_VTB')->get()->first();
-
-        DB::enableQueryLog();
-        $raport = BankRaport::factory()->create([
-            'bank_id' => $bank->id,
-        ]);
-
-        $writer = new ($template->writer)($raport);
-        $writer->write();
-
-        dd($writer, collect(DB::getQueryLog())->map(fn($query) => $query['query']));
-    });
+    Route::get('/test', fn() => Inertia::render('test'));
     Route::get('/view', fn() => Inertia::render('view'));
 }
 
