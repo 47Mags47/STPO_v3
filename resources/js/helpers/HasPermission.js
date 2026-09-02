824a29f0883627ae639bbde79b21de69828bf7a4
mubait <kuz2001min@mail.ru>
2026-09-02 10:08:07 +0700

    fix: appeals hasPermission

--- resources/js/helpers/HasPermission.js
+++ resources/js/helpers/HasPermission.js
@@ -1,12 +1,12 @@
 import { usePage } from "@inertiajs/vue3";
 
-const current_user__permissions = usePage().props?.current_user.data.permissions;
-
 export function hasPermission(code) {
     if (typeof code !== 'string') {
         console.error('Тип параметра функции должен быть string')
         return false
     }
+
+    const current_user__permissions = usePage().props?.current_user.data.permissions;
 
     return current_user__permissions?.some(permission => permission.code === code) ?? false
 }
