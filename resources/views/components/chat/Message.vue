68268ba850141b86273d5a40344c339e6b751caa
mubait <kuz2001min@mail.ru>
2026-09-02 14:37:20 +0700

    upd

--- resources/views/components/chat/Message.vue
+++ resources/views/components/chat/Message.vue
@@ -20,13 +20,6 @@ export default {
         }
     },
 
-    methods:{
-        friendClickHandler() {
-            console.log(this.message)
-            router.get(route('users.show', { user: this.message.sender.id }))
-        }
-    },
-
     computed: {
         current_user: () => usePage().props.current_user?.data,
         date_created_at() {
@@ -45,12 +38,14 @@ export default {
         <div class="flex max-w-[40%] flex flex-col px-4! py-4! rounded-xl gap-2"
             :class="isMine ? 'items-end bg-(--chat-my-message-background-color)' : 'items-start bg-(--chat-other-message-background-color)'">
 
-            <div class="w-full flex items-center gap-2">
+            <!-- <div class="w-full flex items-center gap-2">
                 <BlueButton v-if="!isMine" class="size-[30px]! min-h-0!" :on-click="friendClickHandler">
                     <Ico type="user"/>
                 </BlueButton>
                 <span class="font-bold!"> {{ message.sender.name }} </span>
-            </div>
+            </div> -->
+
+            <span class="font-bold!"> {{ message.sender.name }} </span>
 
             <MessageFile :is-mine="isMine" v-if="message.file" :message="message" />
             <MessageText v-else :message="message.message" />
