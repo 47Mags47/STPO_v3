cbd705857a8ab8586ac5d703de9e9aeb0988dd3e
mubait <kuz2001min@mail.ru>
2026-09-03 09:03:26 +0700

    upd: добавил проп fill в Ico

--- resources/views/components/Ico.vue
+++ resources/views/components/Ico.vue
@@ -6,7 +6,12 @@ export default {
         type: {
             type: String,
             required: true
-        }
+        },
+
+        fill: {
+            type: Boolean,
+            default: false
+        },
     },
 
     computed: {
@@ -23,6 +28,7 @@ export default {
             :is="currentIcon"
             class="size-full"
             :stroke-width="2"
+            :fill="fill ? 'currentColor' : 'none'"
         />
     </div>
 </template>
